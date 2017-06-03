8dc8386d8251cca0e7640f59bc8f15132e544508
Zane Gilmore <zane.gilmore@gmail.com>
2017-06-04 10:58:14 +1200

    fixed url formatting for putchime sync again

--- doorbell_central.go
+++ doorbell_central.go
@@ -312,7 +312,7 @@ func SyncNewDoorbell(w http.ResponseWriter, req *http.Request, ps httprouter.Par
     //fmt.Printf("the files:%s", lsdir)
 
     the_url := fmt.Sprintf("http://%s:%d/putchime",the_server,CONFIG.Satellite_port)
-    the_url = fmt.Sprintf("http://localhost:%s/putchime",CONFIG.Satellite_port)
+    //the_url = fmt.Sprintf("http://localhost:%s/putchime",CONFIG.Satellite_port)
     the_path := ""
     fmt.Printf("the_url:%s\n", the_url)
 
